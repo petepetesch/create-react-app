47b1b36296e2c9716b341dc0fa2ccf40827ee17d
Peter Petesch <petepetesch@hotmail.com>
2021-05-27 14:33:08 -0700

    Update App.js

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Pedro listens well 2!!
+          Edit <code>src/App.js</code> and save to reload. Pedro listens well 3!!
         </p>
         <a
           className="App-link"
