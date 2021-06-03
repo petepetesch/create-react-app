5d23acb80d6bc438cb0bf4fae5d1740da608bfc0
Peter Petesch <petepetesch@hotmail.com>
2021-06-03 10:57:48 -0700

    test update to react app

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Pedro listens well 3!!
+          Edit <code>src/App.js</code> and save to reload. Pedro listens well 3x!!
         </p>
         <a
           className="App-link"
